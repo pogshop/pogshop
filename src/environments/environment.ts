5d5b886a529b3b63d477c564632780462ff76bcb
Alexander Nguyen <alexandernguyen@Alexanders-Laptop.local>
2025-06-15 14:26:31 -0700

    Add payout calculations for upcoming payouts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'https://pogshop.gg',
+  baseUrl: 'http://localhost:4200',
   environment: 'production',
   apiUrl: 'https://api.pogshop.gg',
   stripeApiUrl: 'https://api.pogshop.gg/v1/stripe',
