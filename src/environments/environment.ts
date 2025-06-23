c50d9d083e512c857f40de4c0d8d52578d3e2c9c
Alexander Nguyen <alexandernguyen@Alexanders-Laptop.local>
2025-06-23 16:36:27 -0700

    Fix missing features

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
-  baseUrl: 'http://localhost:4200',
-  environment: 'development',
+  production: true,
+  baseUrl: 'https://pogshop.gg',
+  environment: 'production',
   apiUrl: 'https://api.pogshop.gg',
-  stripeApiUrl: 'https://api.pogshop.gg/v1/test/stripe',
+  stripeApiUrl: 'https://api.pogshop.gg/v1/stripe',
 };
